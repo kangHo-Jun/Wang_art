0ca1f9f26bb449ea35db79d9b905cdff60fadd33
Daesan <zartkang@gmail.com>
2026-06-21 20:16:06 +0900

    build: sync current contact and dist state

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Wang_art/',
+  base: '/',
   publicDir: 'public',
   build: {
     outDir: 'dist',
