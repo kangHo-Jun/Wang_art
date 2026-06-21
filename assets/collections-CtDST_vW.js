00c2b7cdd0baee2be8ca2df7a3c13b23279553cf
Daesan <zartkang@gmail.com>
2026-06-21 20:29:03 +0900

    Updates

--- assets/collections-CtDST_vW.js
+++ assets/collections-CtDST_vW.js
@@ -1,4 +1,4 @@
-function e(){let e=`${`/`.replace(/\/$/,``)}/artist/`,t=document.getElementById(`collectionsContainer`);t&&(t.innerHTML=`
+function e(){let e=`${`/Wang_art/`.replace(/\/$/,``)}/artist/`,t=document.getElementById(`collectionsContainer`);t&&(t.innerHTML=`
       <div class="resume-shell">
         <div class="resume-text resume-text--intro">
           소장처 페이지는 작가 페이지로 통합되었습니다.
